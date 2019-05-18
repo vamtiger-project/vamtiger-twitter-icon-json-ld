5ba3223fce5254cc5a4cb766d65c080830006964
James-Alex Matthews <vamtiger@vamtiger.com>
2019-05-18 16:41:59 +0200

    vamtiger-node-typescript-commit

--- source/json-ld.ts
+++ source/json-ld.ts
@@ -1,9 +1,14 @@
 const jsonLd = {
-    '@context': 'http://schema.org/',
+    "@context": "http://schema.org",
     '@type': 'ImageObject',
     name: 'Twitter HTML/SVG Icon',
     description: 'VAMTIGER HTML/SVG Twitter Icon',
-    author: 'Vamtiger Immortalles',
+    author: {
+        '@type': 'Person',
+        givenName: 'Vamtiger',
+        familyName: 'Immortalles',
+        email: 'vamtiger@vamtiger.com'
+    },
     datePublished: '13 May 2018',
     contentUrl: 'https://unpkg.com/vamtigerr-twitter-icon-json-ld/twitter.html'
 };
--- source/vamtigerr-twitter-icon-json-ld.ts
+++ source/vamtigerr-twitter-icon-json-ld.ts
@@ -6,6 +6,9 @@ import jsonLd from './json-ld';
 import json from './json';
 import loadScript from './load-script';
 
+export {default as jsonLd} from './json-ld';
+export {default as json} from './json';
+
 const jsonLdParams = jsonLd.map(data => ({
     url,
     type: ScriptType.jsonLd,
